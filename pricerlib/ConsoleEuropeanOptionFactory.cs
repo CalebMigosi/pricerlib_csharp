c11b816ba765316d7dc1eba9160b731e2d9807ab
CalebMigosi <calebmigosi@gmail.com>
2021-11-07 10:26:30 +0100

    First commit

---
+++ pricerlib/ConsoleEuropeanOptionFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pricerlib
+{
+    public class ConsoleEuropeanOptionFactory : IOptionFactory
+    {
+        public Option create()
+        {
+            string optionType;
+
+            double r;
+            double vol;
+            double K;
+            double S0;
+            double T;
+            
+            // Cost of carry
+            double b;
+
+            // User input 
+            Console.WriteLine("Spot Price");
+            S0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Strike Price");
+            K = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Volatility");
+            vol = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Interest Rate");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Time to Maturity");
+            T = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Cost of carry");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Option Type: 1.Call , 2.Put");
+            optionType = Convert.ToString(Console.ReadLine());
+
+            // Create option
+            Option opt = new(optionType, S0, K, vol, r, T, b);
+
+            return opt;
+
+        }
+    }
+}
---
+++ pricerlib/Option.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pricerlib
+{
+    public class Option
+    {
+        private double S0, K, vol, r, T, b;
+        private string optionType;
+
+        public Option(string optionType, double S0, double K, double vol, double r, double T, double b)
+        {
+            this.optionType = optionType;
+            this.S0 = S0;
+            this.K = K;
+            this.vol = vol;
+            this.r = r;
+            this.T = T;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Option: \n " +
+                "Option Type: {0} \n " +
+                "Spot: {1} \n " +
+                "Strike: {2} \n" +
+                "Volatility: {3} \n" +
+                "Rate: {4} \n" +
+                "Time to Maturity: {5} \n" +
+                "Cost of carry: {6} \n" +
+                "", optionType, S0, K, vol, r, T, b);
+        }
+    }
+}
